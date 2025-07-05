a70ac1b183e43723d09cbc014188c573a18719ac
MurungaOwen <owenhood80@gmail.com>
2025-07-05 14:02:02 +0300

    some few endpoint for current dashboard

--- karma_ledger_backend/src/karma_event/karma_event.controller.ts
+++ karma_ledger_backend/src/karma_event/karma_event.controller.ts
@@ -14,6 +14,7 @@ import { AuthenticatedRequest } from 'src/util/types';
 import { JwtAuthGuard } from 'src/auth/jwt.guard';
 import { handleError } from 'src/util/error';
 import { ApiBearerAuth } from '@nestjs/swagger';
+
 @Controller('karma-events')
 @ApiBearerAuth('JWT')
 @UseGuards(JwtAuthGuard)
