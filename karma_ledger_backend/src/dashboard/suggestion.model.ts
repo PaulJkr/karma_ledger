82701222d406bcf1f45724362a1dcc4b062eb67d
MurungaOwen <owenhood80@gmail.com>
2025-07-05 14:01:43 +0300

    feat: suggestion model definition

---
+++ karma_ledger_backend/src/dashboard/suggestion.model.ts
@@ -0,0 +1,38 @@
+import {
+  Column,
+  DataType,
+  ForeignKey,
+  Model,
+  PrimaryKey,
+  Table,
+  BelongsTo,
+  Default,
+} from 'sequelize-typescript';
+import { User } from 'src/users/users.model';
+@Table({ tableName: 'karma_suggestions', timestamps: true })
+export class Suggestion extends Model {
+  @PrimaryKey
+  @Default(DataType.UUIDV4)
+  @Column(DataType.UUID)
+  id: string;
+
+  @ForeignKey(() => User)
+  @Column(DataType.UUID)
+  user_id: string;
+
+  @Column(DataType.TEXT)
+  suggestion_text: string;
+
+  @Column(DataType.INTEGER)
+  week: number;
+
+  @Default(false)
+  @Column(DataType.BOOLEAN)
+  used: boolean;
+
+  @Column(DataType.DATE)
+  created_at: Date;
+
+  @BelongsTo(() => User)
+  user: User;
+}
