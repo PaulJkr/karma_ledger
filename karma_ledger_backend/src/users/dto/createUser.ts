bede28ddc9d3ad4df6732d0146838cec0ada7c2f
MurungaOwen <owenhood80@gmail.com>
2025-07-05 11:56:17 +0300

    doc: setup swagger

--- karma_ledger_backend/src/users/dto/createUser.ts
+++ karma_ledger_backend/src/users/dto/createUser.ts
@@ -1,12 +1,23 @@
 import { IsEmail, IsString, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty({ example: 'john_doe', description: 'The username of the user' })
   @IsString()
   username: string;
 
+  @ApiProperty({
+    example: 'john@example.com',
+    description: 'The email address of the user',
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    example: 'strongPassword123',
+    description: 'The password of the user',
+    minLength: 6,
+  })
   @IsString()
   @MinLength(6)
   password: string;
